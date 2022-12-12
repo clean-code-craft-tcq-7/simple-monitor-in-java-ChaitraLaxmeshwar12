4e2a63043544e4d4a60a1d7061137c6988ce57ec
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-12-12 15:49:54 +0530

    Create EVBMSConstants.java

---
+++ vitals/EVBMSConstants.java
@@ -0,0 +1,11 @@
+package vitals;
+
+public class EVBMSConstants {
+
+public static final float maxTemperature = 45f;
+public static final float minTemperature = 0f;
+public static final float maxSoc = 80f;
+public static final float minSoc = 20f;
+public static final float maxChargeRate = 0.8f;
+
+}
