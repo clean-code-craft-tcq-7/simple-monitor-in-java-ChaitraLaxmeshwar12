cbfcf328374169f6b4eddf582caecde15caad872
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-12-12 16:28:35 +0530

    Create EVBMSImpl.java

---
+++ vitals/EVBMSImpl.java
@@ -0,0 +1,33 @@
+package vitals;
+
+public class EVBMSImpl
+{
+  
+  public static boolean isBatteryParameterWithinLimits(float value,float lowerLimit, float upperLimit)
+  {
+    if(value >= lowerLimit && value <= upperLimit) {
+            return false;
+        }
+  }
+  
+  public static boolean isBattTemperatureinLimits(float temperature)
+  {
+    return (isBatteryParameterWithinLimits(temperature,EVBMSConstants.minTemperature,EVBMSConstants.maxTemperature)
+  }
+  
+   public static boolean isBattSOCinLimits(float soc)
+  {
+    return (isBatteryParameterWithinLimits(soc,EVBMSConstants.minSoc,EVBMSConstants.maxSoc)
+  }
+         
+  public static boolean isChargeRateinLimit(float chargeRate)
+   {
+      if(chargeRate >= EVBMSConstants.maxChargeRate)
+        return false;
+   }
+            
+   public static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+     return (isBattTemperatureinLimits(temperature) && isBattSOCinLimits(soc) && isChargeRateinLimit(chargeRate));
+   }
+            
+}
