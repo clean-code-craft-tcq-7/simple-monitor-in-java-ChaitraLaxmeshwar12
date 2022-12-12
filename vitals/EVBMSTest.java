c151fdc8465d585ce9ecb79b0826720cc6579d81
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-12-12 16:41:12 +0530

    Create EVBMSTest.java

---
+++ vitals/EVBMSTest.java
@@ -0,0 +1,11 @@
+package vitals;
+
+public class EVBMSTest
+{
+  
+   public static void EVBMStest
+   {
+   assert(EVBMSImpl.batteryIsOk(25, 70, 0.7f) == true);
+   assert(EVBMSImpl.batteryIsOk(50, 85, 0.0f) == false);
+   } 
+}
