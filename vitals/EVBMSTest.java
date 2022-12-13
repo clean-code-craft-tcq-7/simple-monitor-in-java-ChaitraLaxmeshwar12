539e5aa10e5a54011dc4d2f8f2045f78ad831dd1
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-12-13 19:34:09 +0530

    Update EVBMSTest.java

--- vitals/EVBMSTest.java
+++ vitals/EVBMSTest.java
@@ -6,9 +6,9 @@ public class EVBMSTest
    {
     EVBMSImpl evBMSImpl = new EVBMSImpl();
     assert(evBMSImpl.batteryIsOk(25, 70, 0.7f) == true);
-   // assert(evBMSImpl.batteryIsOk(50, 85, 0.0f) == false);
+    //assert(evBMSImpl.batteryIsOk(50, 85, 0.0f) == false);
       
-    assert(evBMSImpl.batteryIsOk(45, 85, 0.8f) == true);
+    assert(evBMSImpl.batteryIsOk(45, 80, 0.8f) == true);
     assert(evBMSImpl.batteryIsOk(0, 20, 0.8f) == true);
       
     assert(evBMSImpl.batteryIsOk(25, 60, 0.6f) == true);
