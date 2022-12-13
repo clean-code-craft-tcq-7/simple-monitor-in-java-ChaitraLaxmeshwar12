f4a6a48896493bc5d71bf254214206b93b5da9cd
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-12-13 19:39:40 +0530

    Update EVBMSImpl.java

--- vitals/EVBMSImpl.java
+++ vitals/EVBMSImpl.java
@@ -6,13 +6,9 @@ public class EVBMSImpl
   public boolean isBatteryParameterWithinLimits(float value,float lowerLimit, float upperLimit)
   {
     boolean result = false;
-    System.out.println("value->"+value);
-    System.out.println("lowerLimit->"+lowerLimit);
-    System.out.println("upperLimit->"+upperLimit);
     if(value >= lowerLimit && value <= upperLimit) {
             result=true;
         }
-    System.out.println("result->"+result);
     return result;
   }
   
@@ -28,15 +24,10 @@ public class EVBMSImpl
          
   public boolean isChargeRateinLimit(float chargeRate)
    {
-       System.out.println("chargeRate->"+chargeRate);
-       System.out.println(" max chargeRate->"+EVBMSConstants.maxChargeRate);
       return (chargeRate <= EVBMSConstants.maxChargeRate);
    }
             
    public boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-     System.out.println("temp->"+isBattTemperatureinLimits(temperature));
-     System.out.println("soc->"+isBattSOCinLimits(soc));
-     System.out.println("charge->"+isChargeRateinLimit(chargeRate));
      return (isBattTemperatureinLimits(temperature) && isBattSOCinLimits(soc) && isChargeRateinLimit(chargeRate));
    }
             
